eed1ece36f4f458778824a54842edca127a675b9
Jezarr <jezarrilli@hotmail.com>
2025-07-08 19:33:47 -0400

    Initial Commit

---
+++ IsItSpam/IsItSpamApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsItSpamApp.swift
+//  IsItSpam
+//
+//  Created by Jackie Zarrilli on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct IsItSpamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IsItSpamTests/IsItSpamTests.swift
@@ -0,0 +1,16 @@
+//
+//  IsItSpamTests.swift
+//  IsItSpamTests
+//
+//  Created by Jackie Zarrilli on 7/8/25.
+//
+
+import Testing
+
+struct IsItSpamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
